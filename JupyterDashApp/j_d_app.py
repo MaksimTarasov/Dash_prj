36bb3eb6af1c10319d5848e784f61adf5afc9cad
Tarasov Maksim <maksimtarasov1981@gmail.com>
2023-08-29 13:00:20 +0200

    -

--- JupyterDashApp/j_d_app.py
+++ JupyterDashApp/j_d_app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 app = JupyterDash(__name__)
+app.layout = html.Div([dcc.Dropdown(options=[{'label': 'red', 'value': 'red'}])])
 
 if __name__ == '__main__':
-    app.run_server(mode='inline')
+    app.run_server(mode='jupyterlab')
