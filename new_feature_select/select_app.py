100075d95684cbf4ca914be3b6af09b75154bf98
Tarasov Maksim <maksimtarasov1981@gmail.com>
2023-09-04 17:20:13 +0200

    Добвлено чтение csv файла и реализован фильтр по выбранной фамилии

--- new_feature_select/select_app.py
+++ new_feature_select/select_app.py
@@ -18,12 +18,20 @@ def get_data_from_csv(filename:str):
     data_rangs = pd.read_csv(filename)
     return data_rangs
 
+def drop_down_list(data_rangs) -> List:
+    user_list = data_rangs['Unnamed: 2'].tolist()[3:]
+    return user_list
+
+
+
+data_rangs = get_data_from_csv('data/rangs.csv')
+user_list = drop_down_list(data_rangs)
+
 app = dash.Dash(__name__)
-color_list = ['red', 'blue', 'white']
 app.layout = html.Div([
                         dcc.Dropdown(
                             id='color_dropdown',
-                            options=[{'label': color, 'value': color} for color in color_list]),
+                            options=[{'label': user, 'value': user} for user in user_list]),
                         html.Div(id='color_output',)
 ])
 """
@@ -36,16 +44,20 @@ Output - указывается элемент который будет изм�
 """
 @app.callback(Output('color_output', 'children'),
                      Input('color_dropdown', 'value'))
-def display_selected_color(color):
+def display_selected_color(user):
     """
 
-    :param color:
+    :param user:
     :return:
     """
-    if color is None:
-        color = 'nothing'
-    return [html.H3('You selected '), f'{color}']
+    if user is None:
+        user = 'nothing'
+    answer = data_rangs[data_rangs["Unnamed: 2"]==user]
+    return [html.H3('You selected '), f'{answer.values[0][2]}',
+                                      f'{answer.values[0][3]}',
+                                      f'{answer.values[0][5]}'
+
+            ]
 
 if __name__ == '__main__':
-    print(get_data_from_csv('data/rangs.csv')['Unnamed: 2'])
-    #app.run_server()
+    app.run_server()
