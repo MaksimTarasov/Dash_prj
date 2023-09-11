e2a320c36dc6d09a068d4e05333c4e0d9e306bb5
Tarasov Maksim <maksimtarasov1981@gmail.com>
2023-09-11 16:48:18 +0200

    переделал div

--- callbacks/calbacks.py
+++ callbacks/calbacks.py
@@ -1,28 +1,40 @@
 from typing import List
 from dash import Dash, html, dcc
-main_div_style_css = {'display': 'block',
-                      'border': '5px',
-                      'background-color': 'blue',
-                      'text-align': 'center'}
 
-inner_div = {'display': 'inline',
-             'border': '5px',
-             'background-color': 'gray',
-             'text-align': 'center',
-             'width': '100px',
-             'height': '100px'}
+left_panel_div = {
+    'position': 'absolute',
+    'background-color': 'green',
+    'width': '50%',
+    'height': '50%',
+    'left': '0px',
+    'top': '0px'
+}
+right_panel_div = {
+    'position': 'absolute',
+    'background-color': 'blue',
+    'width': '50%',
+    'height': '50%',
+    'left': '50%',
+    'top': '0px'
+}
+inner_div = {
+    'position': 'relative',
+    'background-color': 'gray',
+    'width': '50%',
+    'height': '50%',
+}
 
 app = Dash(__name__)
 app.layout = html.Div(children=[
-    html.Div(children=[
+    html.Div(style=left_panel_div, children=[
         html.Div(id='left_top_div', style=inner_div, children=[html.P('left DIV')]),
         html.Div(id='left_bottom_div', style=inner_div, children=[html.P('right DIV')]),
     ]),
-    html.Div(children=[
+    html.Div(style=right_panel_div, children=[
       html.Div(id='right_top_div', style=inner_div, children=[html.P('left DIV')]),
       html.Div(id='right_bottom_div', style=inner_div, children=[html.P('right DIV')]),
   ])
-], style=main_div_style_css)
+], )
 
 if __name__ == '__main__':
   app.run_server()
